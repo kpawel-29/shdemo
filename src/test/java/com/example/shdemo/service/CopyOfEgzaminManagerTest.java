366dfe3417a19ff86277d6cb99f9ed2e35e57bd2
kpawel-29 <pakadlubowski@gmail.com>
2013-12-17 01:12:40 +0100

    poczatek 5 zadania

--- src/test/java/com/example/shdemo/service/CopyOfEgzaminManagerTest.java
+++ src/test/java/com/example/shdemo/service/CopyOfEgzaminManagerTest.java
@@ -21,25 +21,33 @@ import com.example.shdemo.domain.Student;
 @ContextConfiguration(locations = { "classpath:/beans.xml" })
 @TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
 @Transactional
-public class EgzaminManagerTest {
+public class CopyOfEgzaminManagerTest {
 
 		@Autowired
 		EgzManager egzaminManager;
 
 		private final String PRZEDMIOT_1 = "algebra";
-		private final String PRZEDMIOT_2 = "java";
+		//private final String PRZEDMIOT_2 = "java";
+		private final String PRZEDMIOT_3 = "analiza";
+		private final String PRZEDMIOT_4 = "programowanie";
 
 		private final int ECTS_1  = 12;
-		private final int ECTS_2  = 2; 
+		//private final int ECTS_2  = 2; 
+		private final int ECTS_3  = 11;
+		private final int ECTS_4  = 10;
+		
 
 		private final String NAME_1 = "Marek";
 		private final String NAME_2 = "Piotr";
+		private final String NAME_3 = "Jan";
+		private final String NAME_4 = "Monika";
 
 		private final int INDEKS_1 = 206319;
 		private final int INDEKS_2 = 206318;
+		private final int INDEKS_3 = 206317;
+		private final int INDEKS_4 = 206316;
 		
-		@After
-		@Before
+		//@After
 		public void clear(){
 			List<Student> students = egzaminManager.getAllStudents();
 			List<Egzamin>	egzamins = egzaminManager.getAllEgzamin();
@@ -51,34 +59,61 @@ public class EgzaminManagerTest {
 				egzaminManager.deleteEgzamin(egzamin.getId());
 		}
 		
+		//@Before
+		public void add(){
+			Egzamin egzamin = new Egzamin(PRZEDMIOT_3,ECTS_3);
+			Egzamin egzamin2 = new Egzamin(PRZEDMIOT_4,ECTS_4);
+			
+			Student student = new Student(NAME_3,INDEKS_3);
+			Student student2 = new Student(NAME_4,INDEKS_4);
+
+			egzaminManager.addEgzamin(egzamin);
+			egzaminManager.addEgzamin(egzamin2);
+			int studentId = egzaminManager.addStudent(student);	
+			int studentId2 = egzaminManager.addStudent(student2);		
+			
+			egzaminManager.pinStudentToEgzamin(egzamin.getId(), studentId);
+			egzaminManager.pinStudentToEgzamin(egzamin2.getId(), studentId2);
+			
+
+		
+		}
+		
 		@Test
 		public void addCheck(){
+			add();
+			
 			Egzamin egzamin = new Egzamin(PRZEDMIOT_1,ECTS_1);
 			Student student = new Student(NAME_1,INDEKS_1);
 			
 			egzaminManager.addEgzamin(egzamin);
-			int studentId = egzaminManager.addStudent(student);
+			int studentId = egzaminManager.addStudent(student);		
+			
+			int size = egzaminManager.getEgzaminSrudents(egzamin).size();
+			
 			egzaminManager.pinStudentToEgzamin(egzamin.getId(), studentId);
 			
 			Student retrievedStudent = egzaminManager.getStudent(INDEKS_1);
 			Egzamin retrievedEgzamin = egzaminManager.getEgzamin(PRZEDMIOT_1);
 			List<Student> egzaminStudents = egzaminManager.getEgzaminSrudents(retrievedEgzamin);
-			
+					
 			assertEquals(egzamin, retrievedEgzamin);
 			assertEquals(student,  retrievedStudent);
-			assertEquals(1, egzaminStudents.size());
+			assertEquals(size + 1, egzaminStudents.size());
 			assertEquals(student, egzaminStudents.get(0));			
 		}
 		
 		@Test
-		//@SuppressWarnings("deprecation")
+		@SuppressWarnings("deprecation")
 		public void getCheck(){
 			Egzamin egzamin = new Egzamin(PRZEDMIOT_1, ECTS_1);
 			Student students[] = {new Student(NAME_1, INDEKS_1), new Student(NAME_1, INDEKS_2)};
 			
 			egzaminManager.addEgzamin(egzamin);
 			int studentId[] = {egzaminManager.addStudent(students[0]), egzaminManager.addStudent(students[1])};
-
+			
+			int size = egzaminManager.getEgzaminSrudents(egzamin).size();
+			
 			egzaminManager.pinStudentToEgzamin(egzamin.getId(), studentId[0]);
 			egzaminManager.pinStudentToEgzamin(egzamin.getId(), studentId[1]);
 						
@@ -89,8 +124,9 @@ public class EgzaminManagerTest {
 			long studentsWithName = egzaminManager.countStudentWithName(NAME_1);
 		
 			assertEquals(egzamin, retrievedEgzamin);
-			assertEquals(2, egzaminStudents.size());
-			//assertEquals(students, egzaminStudents.toArray());
+			//assertEquals(students, retrievedStudents);
+			assertEquals(size + 2, egzaminStudents.size());
+			assertEquals(students, egzaminStudents.toArray());
 			assertEquals(1, egzamins.size());
 			assertEquals(egzamin, egzamins.get(0));
 			assertEquals(2, studentsWithName);
@@ -125,13 +161,11 @@ public class EgzaminManagerTest {
 		
 		@Test
 		public void deleteCheck(){
-
+			
 			Egzamin egzamin = new Egzamin(PRZEDMIOT_1,ECTS_1);
-			Egzamin egzamin2 = new Egzamin(PRZEDMIOT_2,ECTS_2);
 			Student students[] = {new Student(NAME_1, INDEKS_1), new Student(NAME_2, INDEKS_2)};
-
+			
 			egzaminManager.addEgzamin(egzamin);
-			egzaminManager.addEgzamin(egzamin2);
 			int studentId[] = {egzaminManager.addStudent(students[0]),
 					egzaminManager.addStudent(students[1])};
 			
@@ -154,12 +188,8 @@ public class EgzaminManagerTest {
 			retrievedEgzamins = egzaminManager.getAllEgzamin();
 			retrievedStudents = egzaminManager.getAllStudents();
 			
-			assertEquals(1, retrievedEgzamins.size());
-			assertEquals(0, retrievedStudents.size());
-			
-			egzaminManager.deleteEgzamin(egzamin2.getId());
-			retrievedEgzamins = egzaminManager.getAllEgzamin();
 			assertEquals(0, retrievedEgzamins.size());
+			assertEquals(0, retrievedStudents.size());
 			
 		}
 		
