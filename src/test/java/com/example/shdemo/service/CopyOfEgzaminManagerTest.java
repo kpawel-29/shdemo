1f1bda5ffd0fa1f792c11f3e137bc3fffb1e7eef
kpawel-29 <pakadlubowski@gmail.com>
2013-12-18 01:00:45 +0100

    delete after and before

--- src/test/java/com/example/shdemo/service/CopyOfEgzaminManagerTest.java
+++ src/test/java/com/example/shdemo/service/CopyOfEgzaminManagerTest.java
@@ -4,8 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
